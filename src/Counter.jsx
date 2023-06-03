4c033bebab4938a6602a21c84880d487782b9237
anasalmezyd <118356463+anasalmezyd@users.noreply.github.com>
2023-06-03 13:48:15 +0300

    Initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+const Counter = () => {
+  return (
+    <div>
+      <button>Increment!</button>
+      <button>Decrement!</button>
+      <button>Reset</button>
+      <h1>Current Count: 0</h1>
+    </div>
+  );
+};
+
+export default Counter;
